c66b16bbfa0a093096eeb4df087af5bcf76cc3a7
Salsa Rahmadati <43996652+salsarahmadati@users.noreply.github.com>
2020-07-09 22:44:07 +0700

    added file

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports={
+    consumer_key:         'DQOnKXPTVd1jvo0Wqak42Lfyq',
+    consumer_secret:      'eVM0MeDINh80NS4nb7YLPDrbId6WhPbyPelk4f61nKYIXtsug5',
+    access_token:         '935886919-HBThsKz0wAAh3kNEdP2GU6ZBknpDlLpC70ps2jbs',
+    access_token_secret:  'J1ctYdjqt4nigVPRSFWi2aeP4Pc8V4syHyETtMXzWUbeW',
+}
